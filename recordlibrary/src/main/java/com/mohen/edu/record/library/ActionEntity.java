aa262590a4241adc6a143dee2055ece12a71d30a
LiKang <kalio0512@sina.com>
2017-10-10 16:34:27 +0800

    test

---
+++ recordlibrary/src/main/java/com/mohen/edu/record/library/ActionEntity.java
@@ -0,0 +1,13 @@
+package com.mohen.edu.record.library;
+
+/**
+ * Created by Kenneth on 2017/10/10.
+ */
+
+public class ActionEntity {
+
+    public double add(double a, double b){
+        return a+b;
+    }
+
+}
